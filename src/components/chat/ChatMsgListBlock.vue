1d759e34e1d6cb7990aa4fbb93be87c5fd40d0f0
陌尘吖 <2827075398@qq.com>
2023-04-21 17:02:11 +0800

    代码优化。

---
+++ src/components/chat/ChatMsgListBlock.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="chat-msg-list-block"></div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/chat/ChatTabsBlock.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="chat-tabs-block"></div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
