277e9a7b6feb5fc748b7834de3a2d54488dea655
陌尘吖 <2827075398@qq.com>
2023-05-10 09:50:08 +0800

    修复latex插件打包问题。

--- src/components/chat/MarkdownBlock.vue
+++ src/components/chat/MarkdownBlock.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MarkdownIt from "markdown-it";
+import MarkdownLatex from "markdown-it-latex";
 import MarkdownItAbbr from "markdown-it-abbr";
 import MarkdownItAnchor from "markdown-it-anchor";
 import MarkdownItFootnote from "markdown-it-footnote";
@@ -8,9 +9,9 @@ import MarkdownItSub from "markdown-it-sub";
 import MarkdownItSup from "markdown-it-sup";
 import MarkdownItTasklists from "markdown-it-task-lists";
 import MarkdownItTOC from "markdown-it-toc-done-right";
-import MarkdownLatex from "markdown-it-latex";
+import {onMounted, ref, watch} from "vue";
 
-defineProps({
+const props = defineProps({
   content: {
     type: String,
     required: true,
@@ -18,21 +19,35 @@ defineProps({
   },
 });
 
-const markdown = new MarkdownIt()
-  .use(MarkdownItAbbr)
-  .use(MarkdownItAnchor)
-  .use(MarkdownItFootnote)
-  .use(MarkdownItHighlightjs)
-  .use(MarkdownItSub)
-  .use(MarkdownItSup)
-  .use(MarkdownItTasklists)
-  .use(MarkdownItTOC)
-  .use(MarkdownLatex);
+let markdown = null;
+const mdHtml = ref('');
+
+onMounted(() => {
+  markdown = new MarkdownIt();
+  let latexPlugin = MarkdownLatex;
+  if (MarkdownLatex.__esModule) {
+    latexPlugin = MarkdownLatex.default;
+  }
+  markdown.use(latexPlugin);
+  markdown.use(MarkdownItAbbr);
+  markdown.use(MarkdownItAnchor);
+  markdown.use(MarkdownItFootnote);
+  markdown.use(MarkdownItHighlightjs);
+  markdown.use(MarkdownItSub);
+  markdown.use(MarkdownItSup);
+  markdown.use(MarkdownItTasklists);
+  markdown.use(MarkdownItTOC);
+  mdHtml.value = markdown.render(props.content);
+});
+
+watch(() => props.content, (newVal, oldVal) => {
+  mdHtml.value = markdown.render(newVal);
+});
 
 </script>
 
 <template>
-  <div v-html="markdown.render(content)"/>
+  <div v-html="mdHtml"/>
 </template>
 
 <style lang="less" scoped>
