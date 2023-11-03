a23803bd3e38056114b8ed2598188ff7f3569305
mochenya <2827075398@qq.com>
2023-11-03 10:37:38 +0800

    修改标签页样式

--- src/components/base/CTabs.vue
+++ src/components/base/CTabs.vue
@@ -36,11 +36,11 @@ const activateTab = (key) => {
   context.emit("update:activeKey", key);
 };
 
-const activeClass = ref("border-gray-400 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 active:bg-gray-400 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:active:bg-gray-600 text-gray-500 dark:text-gray-500");
-const inactiveClass = ref("border-gray-300 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 dark:border-slate-700 dark:bg-gray-900 dark:hover:bg-gray-800 dark:active:bg-gray-700 text-gray-900 dark:text-gray-100");
+const activeClass = ref("border-gray-300 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 dark:border-slate-700 dark:bg-gray-900 dark:hover:bg-gray-800 dark:active:bg-gray-700 text-green-500 dark:text-green-400 font-bold");
+const inactiveClass = ref("border-gray-400 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 active:bg-gray-400 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:active:bg-gray-600 text-gray-500 dark:text-gray-500 font-bold");
 
-const closeActiveClass = ref("hover:bg-gray-400 dark:hover:bg-gray-600");
-const closeInactiveClass = ref("hover:bg-gray-200 dark:hover:bg-gray-700");
+const closeActiveClass = ref("hover:bg-gray-200 dark:hover:bg-gray-700");
+const closeInactiveClass = ref("hover:bg-gray-400 dark:hover:bg-gray-600");
 </script>
 
 <template>
@@ -59,7 +59,7 @@ const closeInactiveClass = ref("hover:bg-gray-200 dark:hover:bg-gray-700");
         </div>
         <div
             class="px-2 py-1.5 mr-1 box-border rounded-md cursor-pointer border border-slate-300 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 dark:border-slate-700  dark:bg-gray-900  dark:hover:bg-gray-800  dark:active:bg-gray-700 select-none">
-          ADD
+          <i class="iconfont icon-add text-xs leading-3 font-bold mx-1"/>
         </div>
       </div>
     </div>
