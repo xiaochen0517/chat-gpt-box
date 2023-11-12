602c87372baf18055d9f5bb20cc5c1c5a53c4372
mochenya <2827075398@qq.com>
2023-11-12 14:07:09 +0800

    添加滑动侧边栏

---
+++ src/components/sidebar/SlideSideBarBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from "vue";
+
+const drawerVisible = ref(false);
+</script>
+
+<template>
+
+  <el-drawer
+      v-model="drawerVisible"
+      title="I am the title"
+      direction="ltr">
+    123
+  </el-drawer>
+</template>
