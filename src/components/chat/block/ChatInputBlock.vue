fbfa3c48410d864b918309a64fac92af17ff5d68
mochenya <2827075398@qq.com>
2023-11-16 13:22:44 +0800

    textarea不可更改高度

--- src/components/chat/block/ChatInputBlock.vue
+++ src/components/chat/block/ChatInputBlock.vue
@@ -67,13 +67,13 @@ const ctrlEnterKeyDown = () => {
   <div class="px-2 py-4 flex flex-row">
     <textarea
         ref="chatInputTextAreaRefs"
-        class="flex-1 h-full m-0 p-2 inline-block bg-gray-100 dark:bg-slate-900 rounded-md box-border border-2 border-gray-300 focus:border-slate-400 dark:border-slate-600 dark:focus:border-slate-400"
+        class="flex-1 h-full m-0 p-2 inline-block bg-gray-100 dark:bg-slate-900 rounded-md box-border border-2 border-gray-300 focus:border-slate-400 dark:border-slate-600 dark:focus:border-slate-400 resize-none"
         v-model="chatInputContent"
         placeholder="Please input message"
         @keydown.enter.prevent.exact="enterKeyDown"
         @keydown.shift.enter.prevent.exact="shiftEnterKeyDown"
         @keydown.ctrl.enter.prevent.exact="ctrlEnterKeyDown"
-        rows="2"/>
+        rows="3"/>
     <div
         @click.stop="commitContent"
         class="w-16 h-full rounded-md flex justify-center items-center ml-2 cursor-pointer hover:bg-slate-200 active:bg-slate-300 dark:hover:bg-slate-700 dark:active:bg-slate-800 border-2 border-slate-200 hover:border-slate-300 active:border-slate-400 dark:border-slate-600">
