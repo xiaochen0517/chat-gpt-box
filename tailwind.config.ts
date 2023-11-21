c6929deb9b01dc80888d29f745d4d0e4458b2b75
mochenya <2827075398@qq.com>
2023-11-21 13:20:13 +0800

    clean package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,16 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        '11': '2.75rem', // 你可以根据需要设置具体的数值
+        '12': '3rem',
+        '13': '3.25rem',
+        '14': '3.5rem',
+        '15': '3.75rem',
+        '16': '4rem',
+      },
+    },
   },
   plugins: [],
 }
