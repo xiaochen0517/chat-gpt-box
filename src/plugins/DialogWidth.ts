cb588a248fa013c96d1a0fc6a5e25e657d191767
mochenya <2827075398@qq.com>
2023-11-25 12:24:47 +0800

    Fixed the default pop-up window width error on mobile devices

--- src/plugins/DialogWidth.ts
+++ src/plugins/DialogWidth.ts
@@ -8,18 +8,24 @@ export default {
       windowWidth.value = window.innerWidth;
     };
     window.addEventListener('resize', updateWidth);
+    // when window width changed, update dialog width
     watch(windowWidth, () => {
-      if (windowWidth.value < 1024) {
+      setAllDialogWidth(windowWidth.value);
+    });
+    const setAllDialogWidth = (width: number) => {
+      if (width < 1024) {
         dialogWidth.value = "90%";
         dialogWidthLg.value = "90%";
       } else {
         dialogWidth.value = "500px";
         dialogWidthLg.value = "70%";
       }
-    });
+    }
     const dialogWidth = ref("500px");
     const dialogWidthLg = ref("700px");
     app.provide('dialogWidth', dialogWidth);
     app.provide('dialogWidthLg', dialogWidthLg);
+    // first init
+    setAllDialogWidth(window.innerWidth);
   },
 };
