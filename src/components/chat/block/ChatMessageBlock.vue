4fdabedd8fdae2169b5648e0023dd43b8d5a53eb
mochenya <2827075398@qq.com>
2023-11-25 11:07:19 +0800

    update message control buttons position

--- src/components/chat/block/ChatMessageBlock.vue
+++ src/components/chat/block/ChatMessageBlock.vue
@@ -50,7 +50,7 @@ const copyMessageContent = () => {
       </div>
     </div>
     <div
-        class="flex flex-row gap-1 mt-2 pointer-events-none opacity-100 lg:opacity-0 group-hover:opacity-100 group-hover:pointer-events-auto">
+        class="flex flex-row gap-1 mt-2 ml-12 pointer-events-none opacity-100 3xl:opacity-0 group-hover:opacity-100 group-hover:pointer-events-auto">
       <button
           class="p-2 rounded-md flex justify-center items-center bg-neutral-50 hover:bg-neutral-200 active:bg-neutral-300 text-neutral-600 hover:text-neutral-700 dark:text-neutral-100 dark:bg-transparent dark:hover:bg-neutral-700 dark:active:bg-neutral-600"
           @click="copyMessageContent">
