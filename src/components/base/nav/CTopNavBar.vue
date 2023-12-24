a4bc60986f18c4646b5fc50bab39b7b704388b0b
mochenya <2827075398@qq.com>
2023-12-24 09:51:37 +0800

    change nav bar button radius size

--- src/components/base/nav/CTopNavBar.vue
+++ src/components/base/nav/CTopNavBar.vue
@@ -27,7 +27,7 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <div class="mx-2 py-3 flex flex-row items-center select-none rounded-b-xl bg-neutral-100 dark:bg-neutral-800">
     <div
-        class="w-8 h-8 ml-4 rounded-xl flex justify-center cursor-pointer bg-neutral-200 dark:bg-neutral-700 hover:dark:bg-neutral-600"
+        class="w-8 h-8 ml-4 rounded-lg flex justify-center cursor-pointer bg-neutral-200 dark:bg-neutral-700 hover:dark:bg-neutral-600"
         @click.stop="$emit('backClick')"
     >
       <i class="iconfont icon-left-arrow text-base leading-8"/>
@@ -35,7 +35,7 @@ const props = withDefaults(defineProps<Props>(), {
     <div class="ml-4 text-lg">{{ props.title }}</div>
     <div v-if="saveButton" class="ml-auto">
       <div
-          class="w-14 mr-4 rounded-xl flex justify-center items-center cursor-pointer bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400 dark:bg-neutral-700 hover:dark:bg-neutral-600 active:dark:bg-neutral-500"
+          class="w-14 mr-4 rounded-lg flex justify-center items-center cursor-pointer bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400 dark:bg-neutral-700 hover:dark:bg-neutral-600 active:dark:bg-neutral-500"
           @click="$emit('saveClick')"
       >
         <div class="text-md leading-8">{{ props.saveButtonText }}</div>
