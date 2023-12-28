cfd208c2e093991cee1906275d16c8d6d926a652
mochenya <2827075398@qq.com>
2023-12-28 10:54:56 +0800

    clean code

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,16 +1,16 @@
-import {createI18n} from 'vue-i18n';
-import en from '../locales/en.json';
-import zh_CN from '../locales/zh-CN.json';
+import {createI18n} from "vue-i18n";
+import en from "../locales/en.json";
+import zh_CN from "../locales/zh-CN.json";
 
 // load locale messages
 const messages = {
   en,
-  'zh-CN': zh_CN,
-}
+  "zh-CN": zh_CN,
+};
 
 const i18n = createI18n({
-  locale: 'zh-CN', // 设置默认语言
-  fallbackLocale: 'en', // 设置备用语言
+  locale: "zh-CN", // 设置默认语言
+  fallbackLocale: "en", // 设置备用语言
   messages, // 设置资源文件
 });
 
