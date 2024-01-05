3f24a995247bedd058e3b083b100dcb6845e6f2b
mochenya <2827075398@qq.com>
2024-01-05 22:34:29 +0800

    Fix auto updater issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,22 @@
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            tauri::async_runtime::spawn(async move {
+                match tauri::updater::builder(handle).check().await {
+                    Ok(update) => {
+                        if update.is_update_available() {
+                            update.download_and_install().await.unwrap();
+                        }
+                    }
+                    Err(e) => {
+                        println!("failed to get update: {}", e);
+                    }
+                }
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
