53eb65ff839a3f0b6db65cb5f2b5234b9cdb8951
mochenya <2827075398@qq.com>
2024-01-27 14:11:18 +0800

    update chat delete dialog

--- src/components/sidebar/ChatBotItem.vue
+++ src/components/sidebar/ChatBotItem.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import {EllipsisOutlined} from "@ant-design/icons-vue";
 import router from "@/router/Router.ts";
-import {ElMessageBox} from "element-plus";
 import {useChatListStore} from "@/store/ChatListStore.ts";
 import {ChatInfo, ChatType} from "@/types/chat/ChatInfo.ts";
 import {onMounted, ref} from "vue";
 import {GoogleGeminiConfig, OpenAiChatGptConfig, OpenAiDallEConfig} from "@/types/chat/BaseConfig.ts";
 import {Avatar, Factory, IAvatarProps} from "vue3-avataaars";
+import {ChatDialogUtil} from "@/utils/dialog/ChatDialogUtil.ts";
+import CBaseDialog from "@/components/base/dialog/CBaseDialog.vue";
 
 type Props = {
   chatInfo: ChatInfo | null,
@@ -46,16 +47,15 @@ const editChatClick = (chatInfo: ChatInfo | null) => {
 };
 
 const chatListStore = useChatListStore();
+const baseDialogRefs = ref<InstanceType<typeof CBaseDialog> | null>(null);
 const deleteChatClick = (chatInfo: ChatInfo | null) => {
-  if (!chatInfo) return;
-  ElMessageBox.confirm(`Are you sure to delete ${chatInfo.name}?`, "Warning", {
-    confirmButtonText: "OK",
-    cancelButtonText: "Cancel",
-    type: "warning",
-  }).then(() => {
-    chatListStore.deleteChat(chatInfo.id);
-  }).catch(() => {
-  });
+  if (!chatInfo || !baseDialogRefs.value) return;
+  ChatDialogUtil.showDeleteChatDialog(baseDialogRefs.value)
+      .then(() => {
+        chatListStore.deleteChat(chatInfo.id);
+      })
+      .catch(() => {
+      });
 };
 </script>
 
@@ -100,5 +100,6 @@ const deleteChatClick = (chatInfo: ChatInfo | null) => {
         </div>
       </template>
     </el-popover>
+    <CBaseDialog ref="baseDialogRefs"/>
   </div>
 </template>
