efc98649c160078c5ac0242b1c1cad017dbbdaa8
mochenya <2827075398@qq.com>
2024-05-20 11:38:12 +0800

    🎉 update top nav bar fixed

--- src/components/base/nav/CTopNavBar.vue
+++ src/components/base/nav/CTopNavBar.vue
@@ -12,23 +12,27 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <div class="mx-4 mt-2 py-1.5 flex flex-row items-center select-none rounded-2xl bg-neutral-100 dark:bg-neutral-900">
-    <div
-        class="w-8 h-8 ml-4 rounded-md flex justify-center cursor-pointer hover:dark:bg-neutral-800 hover:bg-opacity-20 active:dark:bg-neutral-800 active:bg-opacity-80"
-        @click.stop="$emit('backClick')"
-    >
-      <i class="iconfont icon-left-arrow text-base leading-8"/>
-    </div>
-    <div class="ml-4 text-lg font-bold">{{ props.title }}</div>
-    <div v-if="saveButton" class="ml-auto">
-      <div
-          class="w-14 h-8 mr-4 rounded-md flex flex-row justify-center items-center cursor-pointer hover:bg-neutral-200 active:bg-neutral-300 hover:dark:bg-neutral-800 hover:bg-opacity-20 active:dark:bg-neutral-800 active:bg-opacity-80"
-          @click="$emit('saveClick')"
-      >
-        <div v-if="props.saveButtonText && props.saveButtonText.length > 0" class="text-md leading-8">
-          {{ props.saveButtonText }}
+  <div class="w-full h-12">
+    <div class="fixed w-full px-4 pt-2">
+      <div class="py-1.5 flex flex-row items-center select-none rounded-2xl bg-neutral-100 dark:bg-neutral-900">
+        <div
+            class="w-8 h-8 ml-4 rounded-md flex justify-center cursor-pointer hover:dark:bg-neutral-800 hover:bg-opacity-20 active:dark:bg-neutral-800 active:bg-opacity-80"
+            @click.stop="$emit('backClick')"
+        >
+          <i class="iconfont icon-left-arrow text-base leading-8"/>
         </div>
-        <i v-else class="iconfont text-base" :class="props.saveButtonIcon"/>
+        <div class="ml-4 text-lg font-bold">{{ props.title }}</div>
+        <div v-if="saveButton" class="ml-auto">
+          <div
+              class="w-14 h-8 mr-4 rounded-md flex flex-row justify-center items-center cursor-pointer hover:bg-neutral-200 active:bg-neutral-300 hover:dark:bg-neutral-800 hover:bg-opacity-20 active:dark:bg-neutral-800 active:bg-opacity-80"
+              @click="$emit('saveClick')"
+          >
+            <div v-if="props.saveButtonText && props.saveButtonText.length > 0" class="text-md leading-8">
+              {{ props.saveButtonText }}
+            </div>
+            <i v-else class="iconfont text-base" :class="props.saveButtonIcon"/>
+          </div>
+        </div>
       </div>
     </div>
   </div>
