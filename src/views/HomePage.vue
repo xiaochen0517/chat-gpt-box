f6f8c18d3c35bdda96c2d10c5e4307b2934c2415
mochenya <2827075398@qq.com>
2024-09-23 17:50:47 +0800

    ✨ Check the latest version with custom code ✨ Add a unit test framework -- vitest ❌ Removed the automatic latest version check configuration in Tauri

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,22 +1,40 @@
 <script setup lang="ts">
-import {nextTick, ref} from "vue";
+import {nextTick, onMounted, ref} from "vue";
 import DefaultSideBarComponent from "@/components/sidebar/DefaultSideBarComponent.vue";
 import HomeChatComponent from "@/components/chat/block/HomeChatComponent.vue";
 
 import {ChatInfo} from "@/types/chat/ChatInfo.ts";
+import NewVersionInfoDialog from "@/components/updater/NewVersionInfoDialog.vue";
+import UpdateUtil from "@/utils/UpdateUtil.ts";
+import {ElMessage} from "element-plus";
 
-const chatContentBlockRefs = ref<InstanceType<typeof HomeChatComponent> | null>(null);
+const chatContentBlockRefs = ref<InstanceType<typeof HomeChatComponent>>(null);
 const changeChatClick = (chatInfo: ChatInfo) => {
   nextTick(() => {
     if (!chatContentBlockRefs.value) return;
     chatContentBlockRefs.value.changeChat(chatInfo);
   });
 };
+
+const newVersionInfoDialogRef = ref<InstanceType<typeof NewVersionInfoDialog>>(null);
+const checkUpdate = () => {
+  setTimeout(async () => {
+    if (await UpdateUtil.checkUpdate()) {
+      ElMessage.success("New version available");
+      console.log("New version available");
+      newVersionInfoDialogRef.value?.show();
+    }
+  });
+};
+onMounted(() => {
+  checkUpdate();
+});
 </script>
 
 <template>
   <div class="w-full h-full flex flex-row box-border">
     <DefaultSideBarComponent class="hidden mobile:flex" @changeChatClick="changeChatClick"/>
     <HomeChatComponent class="flex-1" ref="chatContentBlockRefs"/>
+    <NewVersionInfoDialog ref="newVersionInfoDialogRef"/>
   </div>
 </template>
